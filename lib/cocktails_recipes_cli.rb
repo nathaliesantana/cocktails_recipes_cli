adb151c2ea759146fcee75f5bf43b130775b9803
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-08 16:14:35 +0000

    Create relative files

--- lib/cocktails_recipes_cli.rb
+++ lib/cocktails_recipes_cli.rb
@@ -8,7 +8,7 @@ end
 
 
 
-require_relative "./cocktails_recipes_cli/reciper"
+require_relative "./cocktails_recipes_cli/recipes"
 require_relative "./cocktails_recipes_cli/scraper"
 require_relative "./cocktails_recipes_cli/version"
 require_relative "./cocktails_recipes_cli/cli"
