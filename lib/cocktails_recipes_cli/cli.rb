b15f86ab94b7f5ff6e68d0cb314a458f54cc98da
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-08 16:34:13 +0000

    Change main_menu to list menu

--- lib/cocktails_recipes_cli/cli.rb
+++ lib/cocktails_recipes_cli/cli.rb
@@ -3,10 +3,10 @@ class  CocktailsRecipesCli::Cli
   def welcome
     CocktailRecipesCli::Scraper.new("https://www.foodandwine.com/cocktail-recipes").scrape
     puts "Hi, Welcome to Reinvented Classic Cocktails"
-    main_menu
+    list_menu
   end
 
-  def main_menu
+  def list_menu
     puts "Here is a list of all of our cocktails:"
     display_cocktails_list
     puts "Type the number of the cocktail you would like to learn."
