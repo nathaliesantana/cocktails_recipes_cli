f5326bfe44c0e62ad586852c8b2a57759be3f340
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-08 16:31:06 +0000

    Define welcome method in Cli

--- lib/cocktails_recipes_cli/cli.rb
+++ lib/cocktails_recipes_cli/cli.rb
@@ -1,3 +1,10 @@
 class  CocktailsRecipesCli::Cli
 
+  def welcome
+    CocktailRecipesCli::Scraper.new("https://www.foodandwine.com/cocktail-recipes").scrape
+    puts "Hi, Welcome to Reinvented Classic Cocktails"
+    main_menu
+  end
+
+
 end
