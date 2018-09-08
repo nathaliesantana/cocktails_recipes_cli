981ec5bc29440b3fd33df0533bc1816a1752dce2
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-08 19:18:39 +0000

    Add iterator class for css

--- lib/cocktails_recipes_cli/scraper.rb
+++ lib/cocktails_recipes_cli/scraper.rb
@@ -9,5 +9,12 @@ end
 def scrape_homepage
   recipes = []
   site= Nokogiri::HTML(open(self.cocktails_page))
+  site.css("li.rsw-teaser").each do |li|
+    recipe_hash = {}
+    recipe_hash[:name]= li.css("h3").text
+    recipe_hash[:url]= "https://www.foodandwine.com#{li.css("a").attribute('href').text}"
+    recipes.push(recipe_hash)
+  end
+  CocktailRecipes::Recipes.create_from_array(recipes)
 
 end
