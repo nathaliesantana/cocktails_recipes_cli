c546de2e03f1399c20be071a44a1df148a9993a1
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-08 19:26:10 +0000

    Define scrape method

--- lib/cocktails_recipes_cli/scraper.rb
+++ lib/cocktails_recipes_cli/scraper.rb
@@ -4,34 +4,38 @@ class CocktailsRecipesCli::Scraper
   def initialize(url)
    self.cocktails_page = url
   end
-end
 
-def scrape_homepage
-  recipes = []
-  site= Nokogiri::HTML(open(self.cocktails_page))
-  site.css("li.rsw-teaser").each do |li|
-    recipe_hash = {}
-    recipe_hash[:name]= li.css("h3").text
-    recipe_hash[:url]= "https://www.foodandwine.com#{li.css("a").attribute('href').text}"
-    recipes.push(recipe_hash)
-  end
-  CocktailRecipes::Recipes.create_from_array(recipes)
-end
-
-def scrape_profile
-  CocktailRecipes::Recipes.all.each do |recipe|
-    site= Nokogiri::HTML(open(recipe.url))
-    hash = {}
-
-    ingrds = site.css('div.ingredients').text
-    if ingrds.count("\r") != 0
-      recipe.ingredients = ingrds.gsub("\r", "_").split.join("\n").gsub("\n", " ").split("_").map {|string| string.split.join(" ") }
-    else
-      recipe.ingredients = ingrds.split("\n").map { |string| string.gsub(/\s+(?=\d)/, "") }.delete_if{|str| str[0] == " "}.reject { |c| c.empty? }
+  def scrape_homepage
+    recipes = []
+    site= Nokogiri::HTML(open(self.cocktails_page))
+    site.css("li.rsw-teaser").each do |li|
+      recipe_hash = {}
+      recipe_hash[:name]= li.css("h3").text
+      recipe_hash[:url]= "https://www.foodandwine.com#{li.css("a").attribute('href').text}"
+      recipes.push(recipe_hash)
     end
-    recipe.instructions = site.css('div.step p').tex
-
+    CocktailRecipes::Recipes.create_from_array(recipes)
   end
-end
+
+  def scrape_profile
+    CocktailRecipes::Recipes.all.each do |recipe|
+      site= Nokogiri::HTML(open(recipe.url))
+      hash = {}
+
+      ingrds = site.css('div.ingredients').text
+      if ingrds.count("\r") != 0
+        recipe.ingredients = ingrds.gsub("\r", "_").split.join("\n").gsub("\n", " ").split("_").map {|string| string.split.join(" ") }
+      else
+        recipe.ingredients = ingrds.split("\n").map { |string| string.gsub(/\s+(?=\d)/, "") }.delete_if{|str| str[0] == " "}.reject { |c| c.empty? }
+      end
+      recipe.instructions = site.css('div.step p').tex
+
+    end
+  end
+
+  def scrape
+    scrape_homepage
+    scrape_profile
+  end
 
 end
